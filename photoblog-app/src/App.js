da73b3565d282e69cd816750847102781b0d39e6
Br0Br0meliad <101954615+Br0Br0meliad@users.noreply.github.com>
2022-10-23 16:29:03 -0600

    start

---
+++ photoblog-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Menu from ;
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Images />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
